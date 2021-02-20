2737b796baa08e022e24c5ebcae21714ad9e88d9
Denis Pessoa <dnspessoa1@gmail.com>
2021-02-20 17:55:27 -0300

    Add save local storage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Todo } from 'src/models/todo.models';
 
 @Component({
   selector: 'app-root',
@@ -6,15 +8,56 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  public todos: any[] = [];
+
+  public form: FormGroup;
+
+  public todos: Todo[] = [];
 
   public title: String = 'Minhas Tarefas';
   /**
    *
    */
-  constructor() {
-    this.todos.push('pos 1');    
-    this.todos.push('pos 2');    
-    this.todos.push('pos 3');    
+  constructor(private formBuilder: FormBuilder) {
+    this.form = this.formBuilder.group({
+      title: ['', Validators.compose([
+        Validators.minLength(3),
+        Validators.maxLength(60),
+        Validators.required
+      ])]
+    });
+
+    // this.todos.push(new Todo(1, 'Tarefa 1', false));    
+    // this.todos.push(new Todo(2, 'Tarefa 2', false));    
+    // this.todos.push(new Todo(3, 'Tarefa 3', true));    
+  }
+
+  add(){
+    //this.form.value => {title: 'titulo'}
+    const title = this.form.controls['title'].value;
+    const id = this.todos.length + 1;
+    this.todos.push(new Todo(id, title, false));
+    this.save();
+    this.clear();
+  }
+
+  clear()
+  {
+    this.form.reset();
+  }
+
+  remove(todo: Todo){
+      const index = this.todos.indexOf(todo);
+      if (index !== -1) {
+        this.todos.splice(index, 1);
+      }
+  }
+
+  markAsDone(todo: Todo){
+    todo.done = true;
+  }
+
+  save () {
+    const data = JSON.stringify(this.todos);
+    localStorage.setItem('todos', data);
   }
 }
